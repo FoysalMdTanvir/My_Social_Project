94f74aaa9139e71fdbe61f56b670b0116c17d16e
MdTanvirFoysal <tanvirfaisal54@gmail.com>
2021-01-28 13:54:30 +0600

    Display posted Image

--- App_Posts/views.py
+++ App_Posts/views.py
@@ -2,14 +2,18 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 from django.contrib.auth.decorators import login_required
 
-from App_Login.models import UserProfile
+from App_Login.models import UserProfile, Follow
 from django.contrib.auth.models import User
+
+from App_Posts.models import Post
 # Create your views here.
 
 
 @login_required
 def home(request):
+    following_list = Follow.objects.filter(follower=request.user)
+    posts = Post.objects.filter(author__in=following_list.values_list('following'))
     if request.method == 'GET':
         search = request.GET.get('search', '')
         result = User.objects.filter(username__icontains=search)
-    return render(request, 'App_Posts/home.html', context={'title': 'Home | iBook', 'search': search, 'result': result})
+    return render(request, 'App_Posts/home.html', context={'title': 'Home | iBook', 'search': search, 'result': result, 'posts': posts})
